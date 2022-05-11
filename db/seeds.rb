2be68f3342ad3921810e9e51667293fe023187c3
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-05-11 16:46:45 -0400

    Index page style

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.delete_all
+
+Flat.create({ name:'Tiny but beauty', address:'Tiny Street', price_per_night:90,number_of_guests:2 })
+Flat.create({ name:'Medium but humble', address:'Humble Street', price_per_night:120,number_of_guests:4 })
+Flat.create({ name:'Large is better', address:'Large Street', price_per_night:160,number_of_guests: 6 })
+Flat.create({ name:'Gigansteskeuuh Appart', address:'Giga Street', price_per_night:2000,number_of_guests: 12 })
